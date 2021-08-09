65614a4d1170ef8c7ff37d98995a39f1959da49c
jmcdagsasc <82073613+jmcdagsasc@users.noreply.github.com>
2021-08-09 14:40:39 -0500

    exercises

--- functions/getcategories.js
+++ functions/getcategories.js
@@ -1,13 +1,16 @@
 const axios = require("axios");
 
 exports.handler = function (event, context, callback) {
-  const ORDERING_URL =
-    "https://apiv4.ordering.co/v400/en/farmazone/business/411/categories";
+  const ORDERING_URL = `https://apiv4.ordering.co/v400/en/farmazone/business/411/categories`;
 
   // Función que limpia la data y la ordena
   const sendCleanData = () => {
-    // Incluir la lógica de la función para que solo obtenga id, business_id, name, enabled
-    // NO queremos products
+    // EJERCICIO 1: Incluir la lógica de la función para que solo obtenga id, business_id, name, enabled, img
+    //      NO queremos products
+    // EJERCICIO 2: Hacer que se pueda consultar dinámicamente cualquier business
+    // EJERCICIO 3: Intentar romper la lambda y canalizar los errores para que se entregue
+    //      info legible y útil al front end
+    // EJERCICIO 4: Realizar un README con documentación de todas las lambdas y su uso
   };
 
   // Función que conjunta datos de la API y responde
